e9b6f49abaa797a59574eb455118d9f233cd6bfc
appropose023-tech <appropose023@gmail.com>
2026-03-30 16:47:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
-import 'camera_live.dart';
+import 'live_overlay.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PCBApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PCBApp extends StatelessWidget {
+  const PCBApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: "PCB Inspector",
       debugShowCheckedModeBanner: false,
-      home: CameraLive(),
+      home: LiveOverlayScreen(),
     );
   }
 }
