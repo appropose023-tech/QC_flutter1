bb7fe994351b16e5d9325cc47178b64a711d3546
appropose023-tech <appropose023@gmail.com>
2026-03-30 16:50:06 +0530

    Update and rename models.dart to defect_box.dart

---
+++ lib/defect_box.dart
@@ -0,0 +1,21 @@
+class DefectBox {
+  final double x, y, w, h;
+
+  DefectBox(this.x, this.y, this.w, this.h);
+
+  // Scale from golden (4032×3024) → camera preview
+  Rect scaleToPreview(double previewW, double previewH) {
+    const double goldenW = 4032;
+    const double goldenH = 3024;
+
+    double scaleX = previewW / goldenW;
+    double scaleY = previewH / goldenH;
+
+    return Rect.fromLTWH(
+      x * scaleX,
+      y * scaleY,
+      w * scaleX,
+      h * scaleY,
+    );
+  }
+}
