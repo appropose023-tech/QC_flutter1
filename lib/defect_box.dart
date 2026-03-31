eea3413b020ce4a5ef8c59f198bcedebf2ae1d56
appropose023-tech <appropose023@gmail.com>
2026-03-31 12:58:41 +0530

    Update defect_box.dart

--- lib/defect_box.dart
+++ lib/defect_box.dart
@@ -1,25 +1,40 @@
-import 'package:flutter/material.dart';
 import 'dart:ui';
 
 class DefectBox {
-  final double x, y, w, h;
+  final double x;
+  final double y;
+  final double w;
+  final double h;
 
-  DefectBox({required this.x, required this.y, required this.w, required this.h});
+  DefectBox({
+    required this.x,
+    required this.y,
+    required this.w,
+    required this.h,
+  });
 
-  // BACKEND image resolution (your actual PCB image)
+  // Backend image resolution
   static const double backendWidth = 3679;
   static const double backendHeight = 1717;
 
-  // Convert backend-coordinates → camera-preview coordinates
-  Rect scaleToPreview(double previewWidth, double previewHeight) {
-    double scaleX = previewWidth / backendWidth;
-    double scaleY = previewHeight / backendHeight;
+  Rect scaleToPreview(double previewW, double previewH) {
+    double sX = previewW / backendWidth;
+    double sY = previewH / backendHeight;
 
     return Rect.fromLTWH(
-      x * scaleX,
-      y * scaleY,
-      w * scaleX,
-      h * scaleY,
+      x * sX,
+      y * sY,
+      w * sX,
+      h * sY,
+    );
+  }
+
+  factory DefectBox.fromJson(Map<String, dynamic> json) {
+    return DefectBox(
+      x: json["x"].toDouble(),
+      y: json["y"].toDouble(),
+      w: json["w"].toDouble(),
+      h: json["h"].toDouble(),
     );
   }
 }
