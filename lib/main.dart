84837680449927ac8eae378afb69828e39eeb702
appropose023-tech <appropose023@gmail.com>
2026-03-31 12:57:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
-import 'live_overlay.dart';
+import 'live_overlay_screen.dart';
 
-void main() {
-  runApp(const PCBApp());
-}
-
-class PCBApp extends StatelessWidget {
-  const PCBApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "PCB Inspector",
-      debugShowCheckedModeBanner: false,
-      home: LiveOverlayScreen(),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(
+    home: LiveOverlayScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
